a9bbc22c0dcb6145922998c32869f9fa1a75d9ee
Saron <saron.assfaw@gmail.com>
2024-03-25 20:10:34 +0100

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,3 @@
-// // import { fileURLToPath, URL } from 'node:url'
-
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
- 
-//   base: 'aroundtheworld/', 
-//   plugins: [vue()]
-//   // resolve: {
-//   //   alias: {
-//   //     '@': fileURLToPath(new URL('./src', import.meta.url))
-//   //   }
-//   // }
-// })
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
